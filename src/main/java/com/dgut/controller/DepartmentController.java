ba8b84da93acfef6dbc4a4a835f8333b6f914ec1
李贵超帅哥 <751609938>
2019-08-29 15:08:48 +0800

    部门权限控制更新

--- src/main/java/com/dgut/controller/DepartmentController.java
+++ src/main/java/com/dgut/controller/DepartmentController.java
@@ -2,14 +2,17 @@ package com.dgut.controller;
 
 
 import com.dgut.jsonBean.DepartmentBean;
+import com.dgut.jsonBean.Manager;
 import com.dgut.service.DepartmentService;
 import com.dgut.service.GetDepPower;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -22,7 +25,11 @@ public class DepartmentController {
     private GetDepPower getDepPower;
 
     @RequestMapping("/department")
-    public List<DepartmentBean> getDepartmentList(){
-        return departmentService.findAll();
+    public List<DepartmentBean> getDepartmentList(@SessionAttribute Manager manager){
+        List<DepartmentBean> data = departmentService.findAll();
+        String depPower = getDepPower.getDepPower(manager.getUsername());
+        StringBuilder sb = new StringBuilder(depPower).reverse();
+        data = data.stream().filter(bean ->sb.charAt(Integer.parseInt(bean.getValue()))=='1').collect(Collectors.toList());
+        return data;
     }
 }
