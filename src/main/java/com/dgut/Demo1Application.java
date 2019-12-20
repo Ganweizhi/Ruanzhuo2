f7b6ee1990d9ec301cc7b3d9d85772ce900204e8
GWZ <858161344@qq.com>
2019-12-20 11:21:03 +0800

    软需框架

--- src/main/java/com/dgut/Demo1Application.java
+++ src/main/java/com/dgut/Demo1Application.java
@@ -1,15 +1,11 @@
 package com.dgut;
 
-import com.dgut.email.MailServiceUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
-@MapperScan("com.dgut.mapper")
 @EnableScheduling
 public class Demo1Application {
 
--- src/main/java/com/dgut/mapper/fileUpload.java
+++
@@ -1,7 +0,0 @@
-package com.dgut.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface fileUpload {
-}
