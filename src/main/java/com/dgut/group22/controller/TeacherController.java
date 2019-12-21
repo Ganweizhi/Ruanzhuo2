e059ec2ee21ccc75af52d962699b4f769ce423b5
黄海宁 <553370109@qq.com>
2019-12-21 16:55:57 +0800

    Ning

--- src/main/java/com/dgut/group22/controller/TeacherController.java
+++ src/main/java/com/dgut/group22/controller/TeacherController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//Ning
 @RestController
 @Transactional
 @RequestMapping("/Teacher")
@@ -24,6 +25,7 @@ public class TeacherController {
         jsonObject.put("teacher",teacher);
         return jsonObject.toJSONString();
     }
+
 
 
 }
