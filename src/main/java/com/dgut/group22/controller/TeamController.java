a795752285a3715e618ddf97f1e1e1b96583e30d
黄海宁 <553370109@qq.com>
2019-12-21 14:22:09 +0800

    Ning

--- src/main/java/com/dgut/group22/controller/TeamController.java
+++ src/main/java/com/dgut/group22/controller/TeamController.java
@@ -39,19 +39,11 @@ public class TeamController {
         return jsonObject.toJSONString();
     }
 
-    @RequestMapping(value = "/findTeacherByTeamId/{team_id}&{page}",method = {RequestMethod.POST})
-    public String findTeacherByTeamId(@PathVariable("team_id")String team_id,@PathVariable("page") String page){
-        int anInt = Integer.parseInt(page);
-        List<Teacher> teachers=new ArrayList<>();
+    @RequestMapping(value = "/findTeacherByTeamId/{team_id}",method = {RequestMethod.POST})
+    public String findTeacherByTeamId(@PathVariable("team_id")String team_id){
         List<Teacher> allTeachers=teamService.findTeacherByTeamId(team_id);
-        for(int i=(anInt-1)*9; i<(anInt-1)*9+9 && i<allTeachers.size(); i++){
-            teachers.add(allTeachers.get(i));
-        }
         JSONObject jsonObject = new JSONObject();
-        int r = allTeachers.size()%9==0?0:1;
-        jsonObject.put("page",allTeachers.size()/9+r);
-        jsonObject.put("curPage",anInt);
-        jsonObject.put("teachers",teachers);
+        jsonObject.put("teachers",allTeachers);
         return jsonObject.toJSONString();
     }
 }
