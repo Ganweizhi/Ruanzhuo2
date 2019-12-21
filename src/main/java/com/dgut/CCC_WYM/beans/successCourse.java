6bc73d217cf79003aff9e0a1a2c6e88a88033a0d
陈传川 <986609648@qq.com>
2019-12-21 14:35:26 +0800

    ccc

---
+++ src/main/java/com/dgut/CCC_WYM/beans/successCourse.java
@@ -0,0 +1,137 @@
+package com.dgut.CCC_WYM.beans;
+
+import javax.xml.crypto.Data;
+
+public class successCourse {
+    private int success_id;
+    private int course_id;
+    private int teacher_id;
+    private Data time;
+    private String success_myself;
+    private String success_student;
+    private String success_society;
+    private String success_evaluation;
+    private String success_wechat;
+    private String success_QQ;
+    private String success_port;
+    successCourse(){};
+
+    public int getSuccess_id() {
+        return success_id;
+    }
+
+    public void setSuccess_id(int success_id) {
+        this.success_id = success_id;
+    }
+
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int course_id) {
+        this.course_id = course_id;
+    }
+
+    public int getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(int teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public Data getTime() {
+        return time;
+    }
+
+    public void setTime(Data time) {
+        this.time = time;
+    }
+
+    public String getSuccess_myself() {
+        return success_myself;
+    }
+
+    public void setSuccess_myself(String success_myself) {
+        this.success_myself = success_myself;
+    }
+
+    public String getSuccess_student() {
+        return success_student;
+    }
+
+    public void setSuccess_student(String success_student) {
+        this.success_student = success_student;
+    }
+
+    public String getSuccess_society() {
+        return success_society;
+    }
+
+    public void setSuccess_society(String success_society) {
+        this.success_society = success_society;
+    }
+
+    public String getSuccess_evaluation() {
+        return success_evaluation;
+    }
+
+    public void setSuccess_evaluation(String success_evaluation) {
+        this.success_evaluation = success_evaluation;
+    }
+
+    public String getSuccess_wechat() {
+        return success_wechat;
+    }
+
+    public void setSuccess_wechat(String success_wechat) {
+        this.success_wechat = success_wechat;
+    }
+
+    public String getSuccess_QQ() {
+        return success_QQ;
+    }
+
+    public void setSuccess_QQ(String success_QQ) {
+        this.success_QQ = success_QQ;
+    }
+
+    public String getSuccess_port() {
+        return success_port;
+    }
+
+    public void setSuccess_port(String success_port) {
+        this.success_port = success_port;
+    }
+
+    @Override
+    public String toString() {
+        return "successCourse{" +
+                "success_id=" + success_id +
+                ", course_id=" + course_id +
+                ", teacher_id=" + teacher_id +
+                ", time=" + time +
+                ", success_myself='" + success_myself + '\'' +
+                ", success_student='" + success_student + '\'' +
+                ", success_society='" + success_society + '\'' +
+                ", success_evaluation='" + success_evaluation + '\'' +
+                ", success_wechat='" + success_wechat + '\'' +
+                ", success_QQ='" + success_QQ + '\'' +
+                ", success_port='" + success_port + '\'' +
+                '}';
+    }
+
+    public successCourse(int success_id, int course_id, int teacher_id, Data time, String success_myself, String success_student, String success_society, String success_evaluation, String success_wechat, String success_QQ, String success_port) {
+        this.success_id = success_id;
+        this.course_id = course_id;
+        this.teacher_id = teacher_id;
+        this.time = time;
+        this.success_myself = success_myself;
+        this.success_student = success_student;
+        this.success_society = success_society;
+        this.success_evaluation = success_evaluation;
+        this.success_wechat = success_wechat;
+        this.success_QQ = success_QQ;
+        this.success_port = success_port;
+    }
+}
---
+++ src/main/java/com/dgut/CCC_WYM/controller/interaction.java
@@ -0,0 +1,23 @@
+package com.dgut.CCC_WYM.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class interaction {
+
+//    @RequestMapping(path = "/")
+//    public String uploadForQQ()
+//    {
+//        return null;
+//    }
+//    @RequestMapping(path = "/")
+//    public String uploadForWechat() {
+//        return null;
+//    }
+//    @RequestMapping(path = "/")
+//    public String uploadForWeb() {
+//        return null;
+//    }
+}
