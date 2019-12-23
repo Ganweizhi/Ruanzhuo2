2dcc7d4ea52f63376020c06ef7a686f6be95de40
许楷沂 <876956898@qq.com>
2019-12-23 21:43:33 +0800

    教学录像更新-by康杰

---
+++ src/main/java/com/dgut/group22/controller/VideoController.java
@@ -0,0 +1,70 @@
+package com.dgut.group22.controller;
+
+
+import com.dgut.group22.javaBean.Video;
+import com.dgut.group22.javaBean.resource;
+import com.dgut.group22.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/resource")
+public class VideoController {
+
+    @Autowired
+    private VideoService videoService;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private String course_id;
+
+    @RequestMapping(value = "/FindAllCourseName",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Video> FindAllCourseName()
+    {
+        return videoService.FindAllCourseName();
+    }
+
+    @RequestMapping(value = "/FindCourseVideoPath/{course_id}",method = RequestMethod.GET)
+    @ResponseBody
+    public String FindCourseVideoPath( @PathVariable String course_id,HttpServletRequest request, HttpServletResponse response)
+    {
+        int course_id2=Integer.parseInt(course_id);
+       String path=videoService.FindCourseVideoPath(course_id);
+
+        String downloadFilePath =System.getProperty("user.dir");
+//        downloadFilePath = "G:\\复习";
+        downloadFilePath =downloadFilePath+"\\src\\main\\resources\\video\\";
+
+        try {
+            FileInputStream fis = null;
+            OutputStream os = null ;
+            fis = new FileInputStream(downloadFilePath+path);
+            int size = fis.available(); // 得到文件大小
+            byte data[] = new byte[size];
+            fis.read(data); // 读数据
+            fis.close();
+            fis = null;
+            response.setContentType("video/mp4"); // 设置返回的文件类型
+            os = response.getOutputStream();
+            os.write(data);
+            os.flush();
+            os.close();
+            os = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
---
+++ src/main/java/com/dgut/group22/service/VideoService.java
@@ -0,0 +1,26 @@
+package com.dgut.group22.service;
+
+import com.dgut.group22.dao.IVideoDao;
+import com.dgut.group22.javaBean.Video;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VideoService {
+
+    @Autowired
+    private IVideoDao videoDao;
+
+    public List<Video> FindAllCourseName()
+    {
+        return videoDao.FindAllCourseName();
+    }
+
+    public String FindCourseVideoPath(String course_id)
+    {
+        return videoDao.FindCourseVideoPath(course_id);
+    }
+
+}
