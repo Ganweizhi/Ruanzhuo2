a5007a754a4dac33740987b3121d1b53f608130c
陈传川 <986609648@qq.com>
2019-12-28 20:28:38 +0800

    ccc_12/28_3

--- database.sql
+++ database.sql
@@ -299,7 +299,7 @@ CREATE TABLE userb(
 drop table if exists question;
 drop table if exists question;
 create table question(
-                         qesstion_id int primary key NOT NULL auto_increment,
+                         question_id int primary key NOT NULL auto_increment,
                          name varchar(20) not null ,
                          title varchar(255) not null,
                          times date default null,
